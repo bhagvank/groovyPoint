606e43173c8e842c07786c1c11c2f927e3782343
bhagvank <bhagvank@gmail.com>
2016-03-06 20:47:08 +0530

    for.groovy

---
+++ for.groovy
@@ -0,0 +1,12 @@
+ // A String is nothing but a sequence of characters.
+def hello = "Hello"
+for(aChar in hello){
+    println aChar
+}
+
+// A Collection object holding the four seasons.
+def seasons = ["Winter", "Season", "Spring", "Autumn"]
+for(season in seasons){
+    println season
+}
+
