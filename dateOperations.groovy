0012c53b56b06206d2e6d344af7cbc25eec8c66a
bhagvank <bhagvank@gmail.com>
2016-06-08 17:40:18 +0530

    date Operations

---
+++ dateOperations.groovy
@@ -0,0 +1,8 @@
+class Example { 
+   static void main(String[] args) { 
+      Date date = new Date(); 
+      
+      // display time and date using toString() 
+      System.out.println(date.toString()); 
+   } 
+}
